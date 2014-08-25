7dae8d7df978490e0c80840fa299a74a2ab394ea
Dennis Lewandowski <dennis.lewandowski@rwth-aachen.de>
2014-08-25 22:18:29 +0200

    Started implementing FetchedResultsDataSource

---
+++ MultiTimer2/FetchedResultsDataSource.h
@@ -0,0 +1,13 @@
+//
+//  FetchedResultsDataSource.h
+//  MultiTimer2
+//
+//  Created by Dennis Lewandowski on 25/08/14.
+//  Copyright (c) 2014 laewahn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FetchedResultsDataSource : NSObject
+
+@end
---
+++ MultiTimer2/FetchedResultsDataSource.m
@@ -0,0 +1,13 @@
+//
+//  FetchedResultsDataSource.m
+//  MultiTimer2
+//
+//  Created by Dennis Lewandowski on 25/08/14.
+//  Copyright (c) 2014 laewahn. All rights reserved.
+//
+
+#import "FetchedResultsDataSource.h"
+
+@implementation FetchedResultsDataSource
+
+@end
