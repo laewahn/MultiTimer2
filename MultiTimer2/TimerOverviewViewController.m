f4c4b72b284f5e0cc00bc6b4ca200319d9de956c
Dennis Lewandowski <dennis.lewandowski@rwth-aachen.de>
2014-08-25 21:56:27 +0200

    Lighter ViewController
    Removed data source and delegate responsibilities from TimerOverviewViewController

--- MultiTimer2/TimerOverviewViewController.m
+++ MultiTimer2/TimerOverviewViewController.m
@@ -30,23 +30,4 @@
 
 }
 
-#pragma mark - Table View
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-	return 0;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-	return 0;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
-	
-    return cell;
-}
-
 @end
