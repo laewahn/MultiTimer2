90f361bfe1b3efdf10405ff8458a7a4968f90c99
Dennis Lewandowski <dennis.lewandowski@rwth-aachen.de>
2014-08-25 21:41:31 +0200

    Implemented duration formatting

--- MultiTimer2/TimerProfileViewModel.m
+++ MultiTimer2/TimerProfileViewModel.m
@@ -36,7 +36,12 @@
 
 - (NSString *)duration
 {
-	return @"00:34";
+	NSInteger durationInSeconds = floor([self.timerProfile duration]);
+	
+	NSInteger seconds = durationInSeconds % 60;
+	NSInteger minutes = durationInSeconds / 60;
+
+	return [NSString stringWithFormat:@"%02d:%02d", minutes, seconds];
 }
 
 @end
--- MultiTimer2Tests/TimerProfileViewModelTests.m
+++ MultiTimer2Tests/TimerProfileViewModelTests.m
@@ -37,6 +37,28 @@
 	XCTAssertEqualObjects([testViewModel name], timerProfileName, @"Should return the name");
 }
 
+- (void)testTimerProfileViewModelReturnsCorrectlyFormattedDurationLessThan60Seconds
+{
+	testViewModel = [[TimerProfileViewModel alloc] initWithTimerProfile:[self someTimerProfileWithDuration:34]];
+	
+	XCTAssertEqualObjects([testViewModel duration], @"00:34", @"Duration should be formatted with leading zeros");
+}
+
+- (void)testTimerProfileViewModelReturnsCorretlyFormattedDurationForOneMinute
+{
+    testViewModel = [[TimerProfileViewModel alloc] initWithTimerProfile:[self someTimerProfileWithDuration:60]];
+	
+	XCTAssertEqualObjects([testViewModel duration], @"01:00");
+}
+
+- (void)testTimerProfileViewModelReturnsCorrectlyFormattedDurationForMoreThanOneMinute
+{
+    testViewModel = [[TimerProfileViewModel alloc] initWithTimerProfile:[self someTimerProfileWithDuration:133]];
+	
+	XCTAssertEqualObjects([testViewModel duration], @"02:13");
+}
+
+
 # pragma mark Fixtures generation
 
 - (TimerProfile *)someTimerProfile
@@ -47,6 +69,11 @@
 - (TimerProfile *)someTimerProfileWithName:(NSString *)name
 {
 	return [TimerProfile createWithName:name duration:10 managedObjectContext:[self managedObjectTestContext]];
+}
+
+- (TimerProfile *)someTimerProfileWithDuration:(NSTimeInterval)duration
+{
+	return [TimerProfile createWithName:@"Test" duration:duration managedObjectContext:[self managedObjectTestContext]];
 }
 
 @end
